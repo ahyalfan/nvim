75c6f3d866bc6539787b7a84506f51e78fb1211c
ahyalfan <aslibojonegoro.1@gmail.com>
2025-01-28 11:39:58 +0700

    menambahkan catatan for k8 help

--- lua/plugins/language/k8.lua
+++ lua/plugins/language/k8.lua
@@ -7,3 +7,10 @@ return {
     event = "VeryLazy",
   },
 }
+-- lazy = true: Menandakan bahwa plugin ini tidak akan dimuat secara otomatis saat Vim/Neovim dimulai.
+-- Sebaliknya, plugin akan dimuat ketika dipicu oleh suatu kondisi tertentu, seperti perintah atau event.
+--
+-- event = "VeryLazy": Ini menentukan bahwa plugin akan dimuat ketika terjadi event dengan nama VeryLazy.
+-- VeryLazy biasanya mengacu pada pemicu yang sangat terlambat, artinya plugin ini
+-- hanya akan dimuat setelah beberapa hal lain diinisialisasi terlebih dahulu,
+-- untuk menghindari menambah waktu startup.
