6c4d59b249a26ac1a3180decc99ea6f6076a2a9c
ahyalfan <alfandi0857@gmail.com>
2025-04-13 18:34:12 +0700

    update lua config -> menambahkan config path for asdf package manager default path evirontm linux

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Lazy config for asdf package manager
+-- Inject ASDF shims path to $PATH so LSP/tools can find executables like `go`, `gopls`, etc
+local asdf_shims = os.getenv("HOME") .. "/.asdf/shims"
+if not string.find(vim.env.PATH, asdf_shims, 1, true) then
+  vim.env.PATH = asdf_shims .. ":" .. vim.env.PATH
+end
