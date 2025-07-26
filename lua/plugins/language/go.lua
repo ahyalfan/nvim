e0d23de4299222ac533ad85218d303554edade22
ahyalfan <alfandi0857@gmail.com>
2025-07-26 14:41:32 +0700

    chore: disable formater go.nvim

--- lua/plugins/language/go.lua
+++ lua/plugins/language/go.lua
@@ -36,14 +36,16 @@ return {
     },
     config = function(lp, opts)
       require("go").setup(opts)
-      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.go",
-        callback = function()
-          require("go.format").goimports()
-        end,
-        group = format_sync_grp,
-      })
+
+      -- -- menonaktifkan fitur go format di go.nvim, soalnya udah di handle sama gopls atau go lsp
+      -- local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      -- vim.api.nvim_create_autocmd("BufWritePre", {
+      --   pattern = "*.go",
+      --   callback = function()
+      --     require("go.format").goimports()
+      --   end,
+      --   group = format_sync_grp,
+      -- })
 
       -- keymap go impl
       vim.keymap.set("n", "gL", function()
