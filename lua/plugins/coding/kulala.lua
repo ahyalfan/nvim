dfd8d4330fb43b7d0fe71405afb47f3c6db8a3ae
ahyalfan <alfandi0857@gmail.com>
2025-08-19 12:26:04 +0700

    chore: add default keymap kulala and enable fitur export import

--- lua/plugins/coding/kulala.lua
+++ lua/plugins/coding/kulala.lua
@@ -1,17 +1,22 @@
 return {
   {
     "mistweaverco/kulala.nvim",
-    keys = {
-      { "<leader>Rs", desc = "Send request" },
-      { "<leader>Ra", desc = "Send all requests" },
-      { "<leader>Rb", desc = "Open scratchpad" },
-    },
+    -- keys = {
+    --   { "<leader>Rs", desc = "Send request" },
+    --   { "<leader>Ra", desc = "Send all requests" },
+    --   { "<leader>Rb", desc = "Open scratchpad" },
+    -- },
     ft = { "http", "rest" },
     opts = {
       -- your configuration comes here
-      global_keymaps = false,
+      global_keymaps = true, -- false if  you create keymap sendiri
       global_keymaps_prefix = "<leader>R",
       kulala_keymaps_prefix = "",
+
+      -- fitur export import
+      lsp = {
+        formatter = true,
+      },
     },
   },
 }
