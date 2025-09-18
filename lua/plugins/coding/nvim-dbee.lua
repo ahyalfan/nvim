555fbb38560fbe7e7c5c769f125f1e7e95574462
ahyalfan <alfandi0857@gmail.com>
2025-09-18 12:30:29 +0700

    nonaktif nvim-dbee

--- lua/plugins/coding/nvim-dbee.lua
+++ lua/plugins/coding/nvim-dbee.lua
@@ -1,21 +1,21 @@
 -- respurce https://github.com/kndndrj/nvim-dbee?tab=readme-ov-file
 
 return {
-  {
-    "kndndrj/nvim-dbee",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-    build = function()
-      -- Install tries to automatically detect the install method.
-      -- if it fails, try calling it with one of these parameters:
-      --    "curl", "wget", "bitsadmin", "go"
-      require("dbee").install()
-    end,
-    config = function()
-      require("dbee").setup({--[[optional config]]
-        -- dbee_binary_path = vim.fn.stdpath("data") .. "/lazy/nvim-dbee/dbee",
-      })
-    end,
-  },
+  -- {
+  --   "kndndrj/nvim-dbee",
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --   },
+  --   build = function()
+  --     -- Install tries to automatically detect the install method.
+  --     -- if it fails, try calling it with one of these parameters:
+  --     --    "curl", "wget", "bitsadmin", "go"
+  --     require("dbee").install()
+  --   end,
+  --   config = function()
+  --     require("dbee").setup({--[[optional config]]
+  --       -- dbee_binary_path = vim.fn.stdpath("data") .. "/lazy/nvim-dbee/dbee",
+  --     })
+  --   end,
+  -- },
 }
