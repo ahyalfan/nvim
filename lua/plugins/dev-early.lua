7396d68dcdb3e3bed3c5121bf3c4ad80717a51b2
ahyalfan <alfandi0857@gmail.com>
2025-09-18 12:31:59 +0700

    close issue for mason v2 in lazy nvim

--- lua/plugins/dev-early.lua
+++ lua/plugins/dev-early.lua
@@ -1,8 +1,13 @@
 -- issue
 -- https://github.com/LazyVim/LazyVim/issues/6039
 
--- jadi untuk sementara sampai di fix
+-- jadi untuk sementara sampai di fix // mei 2025 // closed issue sept 2025
+-- return {
+--   { "mason-org/mason.nvim", version = "1.11.0" },
+--   { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+-- }
+--
 return {
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+  -- { "mason-org/mason.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
