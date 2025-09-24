91fd3e29f6afb4f4f3a8d5c20a526e5096bfc277
ahyalfan <alfandi0857@gmail.com>
2025-09-24 12:20:49 +0700

    chore: add hlchunk plugin

---
+++ lua/plugins/hlchunk.lua
@@ -0,0 +1,11 @@
+return {
+
+  -- hlchunk (https://github.com/shellRaining/hlchunk.nvim?tab=readme-ov-file)
+  -- {
+  --   "shellRaining/hlchunk.nvim",
+  --   event = { "BufReadPre", "BufNewFile" },
+  --   config = function()
+  --     require("hlchunk").setup({})
+  --   end,
+  -- },
+}
